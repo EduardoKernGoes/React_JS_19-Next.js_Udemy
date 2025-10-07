0adfdea256d839e997ffe21dd01ed78cd0bf23cb
Eduardo Goes <edugoes215@gmail.com>
2025-10-07 15:59:10 -0300

    Aulas 33-39, configuração temas, useState, useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -16,63 +16,43 @@ export function App(){
     return <>
 
         <Conteiners>
-                
             <Logo />
-
         </Conteiners>
 
         <Conteiners>
-
             <Menu />
-
         </Conteiners>
 
         <Conteiners>
-
             <ContDown />
-
         </Conteiners>
 
         <Conteiners>
-
             <form className='form' action="">
-
                 <div className="formRow">
-
                     <DefaultInput
                         labelText='task'
                         id='input'
                         type='text'
                         placeholder='Digite Algo'/>
-
                 </div>
 
                 <div className="formRow">
-
                     <p>Lorem ipsum dolor sit amet.</p>
-
                 </div>
 
                 <div className="formRow">
-
                     <Ciclos />
-
                 </div>
 
                 <div className="formRow">
-
                     <DefaultButton icon={<PlayCircleIcon />}/>
-
                 </div>
-
             </form>
-
         </Conteiners>
 
         <Conteiners>
-            
             <Rodape />
-
         </Conteiners>
 
     </>
--- src/componentes/Menu/index.tsx
+++ src/componentes/Menu/index.tsx
@@ -1,19 +1,50 @@
-import { HistoryIcon, SettingsIcon, HouseIcon, SunIcon } from 'lucide-react'
+import { HistoryIcon, SettingsIcon, HouseIcon, SunIcon, MoonIcon } from 'lucide-react'
 import styles from './styles.module.css'
+import { useState, useEffect } from 'react'
+
+type Temas = 'dark' | 'light'
 
 export function Menu() {
+
+    const [theme, setTheme] = useState<Temas>(() => {
+        const storageTheme = (localStorage.getItem('theme') as Temas) || 'dark'
+        return storageTheme;
+    })
+
+    const temaIcon = {
+
+        dark: <SunIcon />,
+        light: <MoonIcon />
+
+    }
+
+    function mudarTema(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        event.preventDefault()
+
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
+            return nextTheme
+        })
+
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
     return <nav className={styles.menu} >
-        <a className={styles.menuLink} href="#">
+        <a className={styles.menuLink} href="#" aria-label='Ir para home' title='Home'>
             <HouseIcon />
         </a>
-        <a className={styles.menuLink} href="#">
+        <a className={styles.menuLink} href="#" aria-label='Ver histórico' title='Histórico'>
             <HistoryIcon />
         </a>
-        <a className={styles.menuLink} href="#">
+        <a className={styles.menuLink} href="#" aria-label='Configurações' title='Configurações'>
             <SettingsIcon />
         </a>
-        <a className={styles.menuLink} href="#">
-            <SunIcon />
+        <a className={styles.menuLink} href="#" aria-label='Mudar tema' title='Tema' onClick={mudarTema}>
+            {temaIcon[theme]}
         </a>
     </nav>
 }
