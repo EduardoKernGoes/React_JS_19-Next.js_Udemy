cffb5cdb68598e34b52b1a3846d8f1b68c1a9f97
Eduardo Goes <edugoes215@gmail.com>
2025-10-07 12:07:50 -0300

    Aulas 25-29, criação formulário da página inicial e estilos/configurações inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Cabecalho } from './componentes/Cabecalho'
+import { ContDown } from './componentes/ContDown'
 import { Conteiners } from './componentes/Conteiner'
 import { Logo } from './componentes/Logo'
 import { Menu } from './componentes/Menu'
+import { DefaultInput } from './componentes/DefaultInput'
 
 import './styles/global.css'
 import './styles/tema.css'
@@ -10,22 +11,57 @@ export function App(){
     return <>
 
         <Conteiners>
-
-            <Cabecalho>
                 
-                <Logo />
-
-            </Cabecalho>
+            <Logo />
 
         </Conteiners>
 
         <Conteiners>
 
-            <Cabecalho>
+            <Menu />
 
-                <Menu />
+        </Conteiners>
 
-            </Cabecalho>
+        <Conteiners>
+
+            <ContDown />
+
+        </Conteiners>
+
+        <Conteiners>
+
+            <form className='form' action="">
+
+                <div className="formRow">
+
+                    <DefaultInput
+                        labelText='task'
+                        id='input'
+                        type='text'
+                        placeholder='Digite Algo'/>
+
+                </div>
+
+                <div className="formRow">
+
+                    <p>Lorem ipsum dolor sit amet.</p>
+
+                </div>
+
+                <div className="formRow">
+
+                    <p>ciclos</p>
+                    <p>0 0 0 0 0 0 0</p>
+
+                </div>
+
+                <div className="formRow">
+
+                    <button>Enviar</button>
+
+                </div>
+
+            </form>
 
         </Conteiners>
 
---
+++ src/componentes/ContDown/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css'
+
+export function ContDown() {
+    return <div className={styles.container} >
+        00:00
+    </div>
+}
