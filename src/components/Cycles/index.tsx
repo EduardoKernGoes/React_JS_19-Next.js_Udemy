a5486b958eff20d345e08ccd33b0a3314ef6c195
Eduardo Goes <edugoes215@gmail.com>
2025-10-08 17:43:39 -0300

    Aula 59, iteração de elementos JSX e como usar a prop key

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,22 +1,37 @@
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext'
+import { getNextCycle } from '../../utils/getNextCycle'
+import { getNextCycleType } from '../../utils/getNextCycleType'
 import styles from './styles.module.css'
 
 export function Cycles(){
+    const { state } = useTaskContext()
+    const cycleStep = Array.from({length: state.currentCycle})
+    const cycleDescription = {
+        workTime: 'foco',
+        shortBreakTime: 'descanso curto',
+        longBreakTime: 'descanso longo'
+    }
+
+
     return(
         <div className={styles.cycles}>
 
-            <span>Cycles:</span>
+            <span>Ciclos:</span>
 
             <div className={styles.cyclesDots}>
 
-                <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.shortBreakTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.shortBreakTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.shortBreakTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.workTime}`}></span>
-                <span className={`${styles.cyclesDot} ${styles.longBreakTime}`}></span>
-
+                {cycleStep.map((_, index) => {
+                    const nextCycle = getNextCycle(index)
+                    const nextCycleType = getNextCycleType(nextCycle)
+                    return (
+                        <span
+                            key={`${nextCycleType}_${nextCycle}`}
+                            className= {`${styles.cyclesDot} ${styles[nextCycleType]}`}
+                            aria-label= {`Indicador de ciclo: ${cycleDescription[nextCycleType]}`}
+                            title= {`Indicador de ciclo: ${cycleDescription[nextCycleType]}`}
+                        ></span>
+                    )
+                })}
 
             </div>
 
